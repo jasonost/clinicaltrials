b9d0749f87bb182bb702f8e40a2bab1782f5625d
anobel <anobel@anobelodisho.com>
2014-04-19 18:13:26 -0700

    ph241: updated data extraction query
    you guys can ignore, this is for my other class project

--- database/extract_241.sql
+++ database/extract_241.sql
@@ -2,10 +2,13 @@ SELECT
 	cs.nct_id, cs.start_date, cs.completion_date, cs.completion_date_type, cs.firstreceived_date, 
 	cs.brief_title, cs.overall_status, cs.phase, cs.study_type, cs.number_of_arms, cs.number_of_groups, cs.enrollment, cs.is_fda_regulated, 
 	i.INTERVENTION_TYPE,
-	s.agency, s.agency_class, s.any_industry, s.num_sponsors
+	s.agency, s.agency_class, s.any_industry, s.num_sponsors,
+	l.country, l.country_class, l.num_countries,
+	d.intervention_model, d.masking, d.primary_purpose, d.observational_model, d.time_perspective, d.allocation, d.endpoint
 FROM clinical_study cs
 JOIN hiv_studies h ON cs.nct_id = h.nct_id
 JOIN interventions_simple_mod i on i.nct_id = h.nct_id
-JOIN sponsors_mod s ON s.nct_id = h.nct_id;
-
+JOIN sponsors_mod s ON s.nct_id = h.nct_id
+JOIN location_mod l ON l.nct_id = h.nct_id
+JOIN design_mod d on d.nct_id = h.nct_id;
 
