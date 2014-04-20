be51717042f90e81c7a2ac39de0d97b2b5760160
anobel <anobel@anobelodisho.com>
2014-04-19 17:12:09 -0700

    sql to extract data for stats analysis

--- database/extract_241.sql
+++ database/extract_241.sql
@@ -1,27 +1,11 @@
 SELECT 
-	cs.*, 
-	s.agency, s.agency_class, 
-	i.INTERVENTION_TYPE
+	cs.nct_id, cs.start_date, cs.completion_date, cs.completion_date_type, cs.firstreceived_date, 
+	cs.brief_title, cs.overall_status, cs.phase, cs.study_type, cs.number_of_arms, cs.number_of_groups, cs.enrollment, cs.is_fda_regulated, 
+	i.INTERVENTION_TYPE,
+	s.agency, s.agency_class, s.any_industry, s.num_sponsors
 FROM clinical_study cs
 JOIN hiv_studies h ON cs.nct_id = h.nct_id
-JOIN sponsors s ON s.nct_id = h.nct_id
-RIGHT JOIN interventions i on i.nct_id = h.nct_id
-WHERE s.sponsor_type = 'Lead Sponsor';
+JOIN interventions_simple_mod i on i.nct_id = h.nct_id
+JOIN sponsors_mod s ON s.nct_id = h.nct_id;
 
 
-select nct_id, intervention_type, count(*) from interventions
-group by nct_id;
-
-select count(*) from interventions;
-
-select distinct i.nct_id, i.intervention_type 
-from interventions i
-JOIN clinical_study cs on i.nct_id = cs.nct_id
-WHERE cs.study_type = "Interventional";
-
-
-select * from intervention_arm_groups limit 1000;
-
-select distinct study_type from clinical_study;
-
-select
