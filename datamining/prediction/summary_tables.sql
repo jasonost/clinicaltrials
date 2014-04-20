a0ff1a7022090045a3bdec6f51335a441c088cd3
Jason Ost <jasonost@gmail.com>
2014-04-19 17:03:55 -0700

    Datamining files

---
+++ datamining/prediction/summary_tables.sql
@@ -0,0 +1,22 @@
+-- simple summary of interventions table, where any instance of multiple types is classified as Multiple
+create table interventions_simple_mod as
+select nct_id, 
+  case when count(distinct intervention_type) = 1 then min(intervention_type) else 'Multiple' end intervention_type
+from interventions
+group by nct_id;
+
+create index interventions_simple_mod_nct_id_idx on interventions_simple_mod(nct_id);
+
+-- more complex summary of interventions table, where we also remove control and placebo categories to see if the treatment(s) are the same type
+create table interventions_complex_mod as
+select i.nct_id,
+  case when count(distinct intervention_type) = 1 then min(intervention_type)
+       when count(distinct case when arm_group_type not in ('Control','No Intervention','Placebo Comparator','Sham Comparator') then intervention_type end) = 1
+         then min(case when arm_group_type not in ('Control','No Intervention','Placebo Comparator','Sham Comparator') then intervention_type end)
+       else 'Multiple' end intervention_type
+from interventions i 
+  left join intervention_arm_groups ia on i.intervention_id=ia.intervention_id
+  left join arm_groups ag on i.nct_id=ag.nct_id and ia.arm_group_label=ag.arm_group_label
+group by i.nct_id;
+
+create index interventions_complex_mod_nct_id_idx on interventions_complex_mod(nct_id);
