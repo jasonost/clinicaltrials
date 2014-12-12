4a476529e198797d0760f990278a85acb93445b1
Katey Basye <kbasye@gmail.com>
2014-12-12 08:15:56 -0800

    Trial list template

--- ctapp/views.py
+++ ctapp/views.py
@@ -1,5 +1,4 @@
 #!../clinicaltrials/env/bin/python
-
 from ctapp import app
 import flask
 from flask import request
@@ -24,8 +23,23 @@ def home():
 # institution page
 @app.route('/institution')
 def institution():
-    return flask.render_template('institution.html')
-
+    params = request.args
+    if 'inst' in params:
+        db = mongo_connect(mongoip)
+        inst_data = db.institutions.find_one({'inst_id': str(params['inst'])})
+    return flask.render_template('institution.html'#,
+                                # inst_name = inst_data['inst_name'],
+                                # inst_loc = inst_data['inst_loc'],
+                                # inst_img = inst_data['inst_img'],
+                                # inst_summary = inst_data['inst_summary'],
+                                # inst_trials_active = inst_data['inst_trials_active'],
+                                # inst_trials_active_info = inst_data['inst_trials_active_info'],
+                                # inst_researchers = inst_data['inst_researchers'],
+                                # inst_pubs = inst_data['inst_pubs'],
+                                # inst_cond_top = inst_data['inst_cond_top'],
+                                # inst_rating = inst_data['inst_rating'],
+                                # inst_rating_info = inst_rating_info
+                                )
 # condition page
 @app.route('/condition')
 def condition():
@@ -34,14 +48,15 @@ def condition():
         db = mongo_connect(mongoip)
         cond_data = db.conditions.find_one({'cond_id': str(params['cond'])})
         return flask.render_template('condition.html', 
-                                     cond_name=cond_data['cond_name'],
-                                     cond_name_mesh=cond_data['cond_name_mesh'],
-                                     cond_summary=cond_data['cond_summary'],
-                                     cond_synonyms=cond_data['cond_synonyms'],
-                                     cond_trials_active=cond_data['cond_trials_active'],
-                                     cond_inst_top=cond_data['cond_inst_top'],
-                                     cond_name_mesh_info=cond_name_mesh_info
-                                     )
+                                    cond_name=cond_data['cond_name'],
+                                    cond_name_mesh=cond_data['cond_name_mesh'],
+                                    cond_summary=cond_data['cond_summary'],
+                                    cond_synonyms=cond_data['cond_synonyms'],
+                                    trials_active=cond_data['cond_trials_active'],
+                                    cond_inst_top=cond_data['cond_inst_top'],
+                                    cond_name_mesh_info=cond_name_mesh_info,
+                                    inst_trials_active_info="Trials with status recruiting or active."
+                                    )
     else:
         return flask.render_template('index.html')
 
