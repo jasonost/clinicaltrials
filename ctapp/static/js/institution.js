94e96175c60585adb3ce435ee25b192207916877
Jason Ost <jasonost@gmail.com>
2014-12-13 15:32:33 -0800

    This works on my local

--- ctapp/static/js/institution.js
+++ ctapp/static/js/institution.js
@@ -6,14 +6,16 @@ $.getJSON( $SCRIPT_ROOT + "_top_condition", {inst: inst_id}, function( data ) {
   var items = [];
   var i = 1;
   $.each( data.result, function( key, val ) {
-  	var item = data.result[key]
-  			item["sortOrder"] = i++
+  	var item = data.result[key];
+  	item["sortOrder"] = i++;
+    console.log(item);
   	items.push(item);
   });
 
   var chart = new dimple.chart(svg, items);
 			chart.setBounds(125, 20, 360, 350);
 	  	var yAxis = chart.addCategoryAxis("y", "cond_name");
+          yAxis.addOrderRule('trial_count', false);
 	  	var xAxis = chart.addMeasureAxis("x", "trial_count");
 	  			xAxis.title = "Trial Count"
 	  	var chartSeries = chart.addSeries(null, dimple.plot.bar);
--- ctapp/views.py
+++ ctapp/views.py
@@ -1,20 +1,16 @@
 #!../clinicaltrials/env/bin/python
 from ctapp import app
-import flask
-from flask import request
-import pymongo
+import flask, pymongo, time
+from flask import request, url_for
 from connect import mongoip
 
 # initializing global variables
 #mongoip = 'localhost'
 inst_rating_info = 'These currently do not have any meaning.'
-inst_trials_active_info="Trials with status recruiting or active."
-cond_name_mesh_info = 'MeSH is a hierarchy of medical subject headings.'
+inst_trials_active_info = "Trials that are recruiting or active."
+cond_name_mesh_info = 'This is the official Medical Subject Heading (MeSH) term for this condition.'
 
-def mongo_connect(ip):
-    conn = pymongo.MongoClient(host=ip)
-    db = conn.ctdb
-    return db
+db = pymongo.MongoClient(host=mongoip).ctdb
 
 # homepage
 @app.route('/')
@@ -26,53 +22,68 @@ def home():
 def institution():
     params = request.args
     if 'inst' in params:
-        db = mongo_connect(mongoip)
-        inst_data = db.institutions.find_one({'inst_id': str(params['inst'])})
-        return flask.render_template('institution.html',
-                                inst_name=inst_data['inst_name'],
-                                inst_loc=inst_data['inst_loc'],
-                                inst_img=inst_data['inst_img'],
-                                inst_summary=inst_data['inst_summary'],
-                                trials_active=inst_data['inst_trials_active'],
-                                inst_trials_active_info=inst_trials_active_info,
-                                inst_researchers=inst_data['inst_researchers'],
-                                inst_pubs=inst_data['inst_pubs'],
-                                inst_cond_top=inst_data['inst_cond_top'],
-                                inst_rating=inst_data['inst_rating'],
-                                inst_rating_info=inst_rating_info
-                                )
-    else:
-        return flask.render_template('index.html')
+
+        for i in range(5):
+            try:
+                inst_data = db.institutions.find_one({'inst_id': str(params['inst'])})
+                if inst_data:
+                    return flask.render_template('institution.html',
+                                                inst_name=inst_data['inst_name'],
+                                                inst_loc=inst_data['inst_loc'],
+                                                inst_img=inst_data['inst_img'],
+                                                inst_summary=inst_data['inst_summary'],
+                                                trials_active=inst_data['inst_trials_active'],
+                                                inst_trials_active_info=inst_trials_active_info,
+                                                inst_researchers=inst_data['inst_researchers'],
+                                                inst_pubs=inst_data['inst_pubs'],
+                                                inst_cond_top=inst_data['inst_cond_top'],
+                                                inst_rating=inst_data['inst_rating'],
+                                                inst_rating_info=inst_rating_info
+                                                )
+            except:
+                print 'Mongo connection error'
+
+    return flask.redirect(url_for('home'))
 
 # top conditions JSON
 @app.route('/_top_condition')
 def top_condition():
     params = request.args
     if 'inst' in params:
-        db = mongo_connect(mongoip)
-        inst_data = db.institutions.find_one({'inst_id': str(params['inst'])})
-        return flask.jsonify(result=inst_data['inst_cond_top'])
-    else:
-        return flask.render_template('index.html')
+
+        for i in range(5):
+            try:
+                inst_cond_top = db.institutions.find_one({'inst_id': str(params['inst'])})['inst_cond_top']
+                if inst_cond_top:
+                    return flask.jsonify(result=inst_cond_top)
+            except:
+                print 'Mongo connection error'
+
+    return flask.jsonify(None)
 
 # condition page
 @app.route('/condition')
 def condition():
     params = request.args
     if 'cond' in params:
-        db = mongo_connect(mongoip)
-        cond_data = db.conditions.find_one({'cond_id': int(params['cond'])})
-        return flask.render_template('condition.html', 
-                                    cond_name=cond_data['cond_name'],
-                                    cond_name_mesh=cond_data['cond_name_mesh'],
-                                    cond_summary=cond_data['cond_summary'],
-                                    cond_synonyms=cond_data['cond_synonyms'],
-                                    trials_active=cond_data['cond_trials_active'],
-                                    institution_list=cond_data['cond_inst_top'],
-                                    cond_name_mesh_info=cond_name_mesh_info
-                                    )
-    else:
-        return flask.render_template('index.html')
+
+        for i in range(5):
+            try:
+                cond_data = db.conditions.find_one({'cond_id': int(params['cond'])})
+                if cond_data:
+                    return flask.render_template('condition.html', 
+                                                cond_name=cond_data['cond_name'],
+                                                cond_name_mesh=cond_data['cond_name_mesh'],
+                                                cond_summary=cond_data['cond_summary'],
+                                                cond_synonyms=cond_data['cond_synonyms'],
+                                                trials_active=cond_data['cond_trials_active'],
+                                                institution_list=cond_data['cond_inst_top'],
+                                                cond_name_mesh_info=cond_name_mesh_info
+                                                )
+            except:
+                print 'Mongo connection error'
+    
+    return flask.redirect(url_for('home'))
 
 
 # old ClinicalTrials Browser infoviz project
