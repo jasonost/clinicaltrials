c61e95c9b94a96f91beef4561502293a1b95c689
Jason Ost <jasonost@gmail.com>
2015-03-02 14:43:23 -0800

    Updating line break handling
    Incoming line breaks represented as “<br />” instead of replacing with
    a space

--- database/fix_linebreaks.py
+++ database/fix_linebreaks.py
@@ -5,6 +5,9 @@ import os, re
 
 files = os.listdir('.')
 
+def create_breaks(data):
+    return [d.replace('AEIOUAEIOU','<br />').replace('AEIOU',' ') for d in data]
+
 for f in files:
     if f[-3:] == 'txt':
         curfile = open(f,'r')
@@ -19,17 +22,18 @@ for f in files:
             else:
                 # read in this row
                 thisrow = row.strip().split('|')
-                thisrow = [re.sub('^ +| +$','',re.sub(' +',' ',c.replace('\r','').replace('\n',' ').replace('\\',' '))) for c in thisrow]
+                # AEIOU is a placeholder for line breaks...
+                thisrow = [re.sub('^ +| +$','',re.sub(' +',' ',c.replace('\r','').replace('\n','AEIOU').replace('\\',' '))) for c in thisrow]
                 # append to the last list in towrite
                 if len(towrite) == 0:
                     towrite.append(thisrow)
                     newrow = thisrow[:]
                 else:
                     lastrow = towrite[0]
-                    newrow = lastrow[:-1] + [lastrow[-1] + ' ' + thisrow[0]] + thisrow[1:]
+                    newrow = lastrow[:-1] + [lastrow[-1] + 'AEIOU' + thisrow[0]] + thisrow[1:]
                 # if the newrow is the too long, write out the existing element and replace with new row
                 if len(newrow) > numfields:
-                    outfile.write('|'.join(towrite[0]) + '\n')
+                    outfile.write('|'.join(create_breaks(towrite[0])) + '\n')
                     rowcount += 1
                     towrite[0] = thisrow[:]
                 # otherwise replace existing element with newrow
@@ -38,10 +42,10 @@ for f in files:
                     towrite.append(newrow[:])
                     towrite.append(thisrow)
         if len(towrite[0]) > numfields:
-            outfile.write('|'.join(towrite[1]) + '\n')
+            outfile.write('|'.join(create_breaks(towrite[1])) + '\n')
             rowcount += 1
         elif len(towrite[0]) == numfields:
-            outfile.write('|'.join(towrite[0]) + '\n')
+            outfile.write('|'.join(create_breaks(towrite[0])) + '\n')
             rowcount += 1
         print f, rowcount
         curfile.close()
