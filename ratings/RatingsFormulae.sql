5ffe66ba18e942ec2358056e408bfe8c8cf627b9
Jason Ost <jasonost@gmail.com>
2015-03-13 15:58:30 -0700

    Updates to ratings
    Dates, MeSH terms, sites, trial descriptions

--- ratings/RatingsFormulae.sql
+++ ratings/RatingsFormulae.sql
@@ -32,7 +32,8 @@ SELECT
 FROM ratings_dates;
 */
 
-CREATE OR REPLACE VIEW ratings_dates_stars AS
+DROP TABLE IF EXISTS ratings_dates_stars;
+CREATE TABLE ratings_dates_stars AS
 SELECT
   nct_id,
   1 +
@@ -45,6 +46,7 @@ SELECT
   (old_completion_date * 0.5) - 
   (old_primary_completion_date * 0.5) score
 FROM ratings_dates;
+CREATE UNIQUE INDEX ratings_dates_stars_nct_id_idx on ratings_dates_stars(nct_id);
 
 /*
 --testing
@@ -76,13 +78,15 @@ FROM
   intervention_browse i using (nct_id)
 GROUP BY 1;
 
-CREATE OR REPLACE VIEW ratings_mesh_stars AS
+DROP TABLE IF EXISTS ratings_mesh_stars;
+CREATE TABLE ratings_mesh_stars AS
 SELECT
   nct_id,
   1 +
   (least(4,cond_terms) * 0.5) +
   (least(2,intervention_terms)) score
 FROM ratings_mesh;
+CREATE UNIQUE INDEX ratings_mesh_stars_nct_id_idx on ratings_mesh_stars(nct_id);
 
 /*
 --testing
@@ -104,43 +108,57 @@ FROM ratings_mesh_stars;
 CREATE OR REPLACE VIEW ratings_sites AS
 SELECT
   s.nct_id,
-  facility_id site,
-  if(overall_status = 'Recruiting',1,0) recruiting_trial,
-  if(status = 'Recruiting',1,0) recruiting_site,
-  if(length(country) > 0,1,0) has_country,
-  if(length(city) > 0,1,0) has_city,
-  if(country = 'United States',1,0) is_us,
-  if(country = 'United States' and length(zip) > 0,1,0) has_us_zip,
-  if((lower(facility_name) rlike '(investigat[a-z]+|re[search]+|clinical|study|trial|various|multiple|recruit[ing]*|undisclosed|community) site'
-       or lower(facility_name) rlike '^site'
-       or lower(facility_name) rlike 'sites? (# *)?[0-9]+'
-       or lower(facility_name) rlike '[0-9]+ site'
-       or (facility_name like '%information%' and facility_name like '%for %'))
-      and facility_name not like '%universit%',1,0) generic_site,
-  if(length(facility_name) <= 1 or facility_name rlike '^[[:digit:][:punct:][:blank:]]+$',1,0) facility_name_bad,
-  if((length(facility_name) - length(regex_replace(cast(facility_name as CHAR character set ascii),'[[:digit:][:punct:]]',''))) / length(facility_name) > 0.5,1,0) facility_name_numbers
+  sum(if(f.facility_id is not null,1,0)) total_sites,
+  max(if(overall_status = 'Recruiting',1,0)) recruiting_trial,
+  max(if(status = 'Recruiting',1,0)) recruiting_site,
+  sum(if(length(country) > 0,1,0)) has_country,
+  sum(if(length(city) > 0,1,0)) has_city,
+  sum(if(country = 'United States',1,0)) is_us,
+  sum(if(country = 'United States' and length(zip) > 0,1,0)) has_us_zip,
+  sum(if((lower(facility_name) rlike '(investigat[a-z]+|re[search]+|clinical|study|trial|various|multiple|recruit[ing]*|undisclosed|community) site'
+           or lower(facility_name) rlike '^site'
+           or lower(facility_name) rlike 'sites? (# *)?[0-9]+'
+           or lower(facility_name) rlike '[0-9]+ site'
+           or (facility_name like '%information%' and facility_name like '%for %')
+           or length(facility_name) <= 1 
+           or facility_name rlike '^[[:digit:][:punct:][:blank:]]+$'
+           or (length(facility_name) - 
+               length(regex_replace(cast(facility_name as CHAR character set ascii),'[[:digit:][:punct:]]',''))) / 
+              length(facility_name) > 0.5)
+          and facility_name not like '%universit%',1,0)) generic_site_name,
+  sum(if(investigator_id is not null,1,0)) total_investigators,
+  sum(if(length(name_degree) - replace(name_degree,' ','') > 0,1,0)) ok_investigator_name
 FROM 
   clinical_study s
  LEFT JOIN
-  facilities f using (nct_id);
+  facilities f using (nct_id)
+ LEFT JOIN
+  investigators i using (facility_id)
+GROUP BY 1;
 
-CREATE OR REPLACE VIEW ratings_sites_stars_raw AS
+DROP TABLE IF EXISTS ratings_sites_stars;
+CREATE TABLE ratings_sites_stars AS
 SELECT
   nct_id,
-  count(distinct site) total_sites,
-  max(recruiting_trial) recruiting_trial,
-  max(recruiting_site) recruiting_site
+  1 + 
+  case when total_sites > 0 then
+    (has_country / total_sites * 1.5) +
+    (has_city / total_sites * 1.5) -
+    (generic_site_name / total_sites * 1.5) -
+    ((is_us - has_us_zip) / total_sites) -
+    if(recruiting_trial != recruiting_site,0.5,0) +
+    case when total_investigators > 0 then
+      (ok_investigator_name / total_investigators) - 
+      ((1 - least(total_investigators / total_sites,1)) * 0.5)
+      else 0 end
+    else 0 end score
 FROM ratings_sites;
-
-CREATE OR REPLACE VIEW ratings_sites_stars AS
-SELECT
-  nct_id
-FROM ratings_sites_stars_raw
+CREATE UNIQUE INDEX ratings_sites_stars_nct_id_idx on ratings_sites_stars(nct_id);
 
 /*
 --testing
 SELECT
-  score,
+  round(score*2,0)/2 score,
   count(*) total
 FROM ratings_sites_stars
 GROUP BY 1;
@@ -149,6 +167,20 @@ SELECT
   avg(score)
 FROM ratings_sites_stars;
 */
+
+/*
+SELECT
+  a.score dates,
+  b.score mesh,
+  round(c.score*2,0)/2 sites,
+  count(*) total
+FROM ratings_dates_stars a  
+join ratings_mesh_stars b using (nct_id)
+join ratings_sites_stars c using (nct_id)
+GROUP BY 1,2,3;
+*/
+
+
 
 
 
