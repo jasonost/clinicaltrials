8a49f84c34653ab493d16b3ba17d28a1b0a5d9c3
Jason Ost <jasonost@gmail.com>
2015-03-28 23:17:36 -0700

    Switching flask to MySQL
    Updating search and institution page to MySQL

--- ctapp/views.py
+++ ctapp/views.py
@@ -1,16 +1,30 @@
 #!../clinicaltrials/env/bin/python
+# -*- coding: utf-8 -*-
 from ctapp import app
 import flask, pymongo, time
 from flask import request, url_for
-from connect import mongoip, mongouri
+
+# SQLAlchemy setup
+from sqlalchemy import create_engine
+from sqlalchemy.sql import func, select
+from connect import mysqlusername, mysqlpassword, mysqlserver, mysqldbname
+from db_tables import metadata, InstitutionDescription, InstitutionLookup, ConditionDescription, ConditionLookup
+
+def initializeDB(user,pwd,server,dbname):
+    engine = create_engine('mysql://%s:%s@%s/%s' % (user,pwd,server,dbname), pool_recycle=3600)
+    conn = engine.connect()
+    metadata.create_all(engine)
+    return conn
+
+mysqlserver = 'localhost'
+conn = initializeDB(mysqlusername, mysqlpassword, mysqlserver, mysqldbname)
 
 # initializing global variables
-#mongoip = 'localhost'
 inst_rating_info = 'These currently do not have any meaning.'
 inst_trials_active_info = "Trials that are recruiting or active."
 cond_name_mesh_info = 'This is the official Medical Subject Heading (MeSH) term for this condition.'
 
-db = pymongo.MongoClient(mongouri).clinicaltrials
+#db = pymongo.MongoClient(mongouri).clinicaltrials
 
 # homepage
 @app.route('/')
@@ -23,26 +37,34 @@ def home():
 def institution():
     params = request.args
     if 'inst' in params:
-
         for i in range(5):
             try:
-                inst_data = db.institutions.find_one({'inst_id': str(params['inst'])})
+                inst_data = conn.execute(InstitutionDescription.\
+                                            select().\
+                                            where(InstitutionDescription.c.institution_id == int(params['inst']))).\
+                                        fetchone()
+                trial_cnt = conn.execute(select([InstitutionLookup.c.institution_id, 
+                                            func.count(InstitutionLookup.c.nct_id)]).\
+                                            where(InstitutionLookup.c.institution_id == int(params['inst'])).\
+                                            group_by(InstitutionLookup.c.institution_id)).\
+                                        fetchone()
                 if inst_data:
                     return flask.render_template('institution.html',
-                                                inst_name=inst_data['inst_name'],
-                                                inst_loc=inst_data['inst_loc'],
-                                                inst_img=inst_data['inst_img'],
-                                                inst_summary=inst_data['inst_summary'],
-                                                trials_active=inst_data['inst_trials_active'],
-                                                inst_trials_active_info=inst_trials_active_info,
-                                                inst_researchers=inst_data['inst_researchers'],
-                                                inst_pubs=inst_data['inst_pubs'],
-                                                inst_cond_top=inst_data['inst_cond_top'],
-                                                inst_rating=inst_data['inst_rating'],
-                                                inst_rating_info=inst_rating_info
+                                                inst_name=inst_data[1],
+                                                inst_loc=inst_data[3],
+                                                inst_img=inst_data[4],
+                                                inst_summary=inst_data[5].decode('utf-8') or 'No description available',
+                                                #trials_active=inst_data['inst_trials_active'],
+                                                #inst_trials_active_info=inst_trials_active_info,
+                                                #inst_researchers=inst_data['inst_researchers'],
+                                                #inst_pubs=inst_data['inst_pubs'],
+                                                #inst_cond_top=inst_data['inst_cond_top'],
+                                                #inst_rating=inst_data['inst_rating'],
+                                                #inst_rating_info=inst_rating_info
                                                 )
             except:
-                print 'Mongo connection error'
+                print 'Database connection error'
+                initializeDB(mysqlusername, mysqlpassword, mysqlserver, mysqldbname)
 
     return flask.redirect(url_for('home'))
 
@@ -58,7 +80,7 @@ def top_condition():
                 if inst_cond_top:
                     return flask.jsonify(result=inst_cond_top)
             except:
-                print 'Mongo connection error'
+                print 'Database connection error'
 
     return flask.jsonify(None)
 
@@ -82,7 +104,7 @@ def condition():
                                                 cond_name_mesh_info=cond_name_mesh_info
                                                 )
             except:
-                print 'Mongo connection error'
+                print 'Database connection error'
     
     return flask.redirect(url_for('home'))
 
